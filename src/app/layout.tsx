4e5c4fceb110d8b9702808ff7e5bd013916021f9
Your Name <Poorna Hari>
2026-02-12 11:40:10 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Academic Planner",
   description: "Student Productivity Dashboard",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
